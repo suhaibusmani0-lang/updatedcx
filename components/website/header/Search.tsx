511ba4ee965ccc26dd5932ea68ceabeb9105f81c
zarneticgit config --global user.email <suhaibusmani0@gmail.com>
2026-07-30 22:39:31 +0530

    fix: added missing aria-label to search button for 100% accessibility

--- components/website/header/Search.tsx
+++ components/website/header/Search.tsx
@@ -121,7 +121,8 @@ export default function SearchBar() {
             className="flex-1 py-2 outline-none bg-transparent text-sm px-1 text-gray-800"
           />
 
-          <button type="submit" className="p-2 text-gray-700 hover:text-black">
+          {/* YAHAN FIX KIYA HAI: aria-label="Search" add kiya hai */}
+          <button type="submit" aria-label="Search" className="p-2 text-gray-700 hover:text-black">
             <Search size={18} />
           </button>
         </form>
